5ed88582183acae7df676f3eb5456c9a8730771f
Luis Segoviano <the.segoviano@gmail.com>
2022-01-16 23:49:41 -0600

    Initial Commit

---
+++ PruebaTecnicaUPAX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaTecnicaUPAX
+//
+//  Created by Luis Segoviano on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
